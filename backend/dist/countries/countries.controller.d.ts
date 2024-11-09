3f2760e0f1c18a928a4921776eafac4fc2dee5bd
enzojga <enzo202p@gmail.com>
2024-11-09 18:07:44 -0300

    chore: removing useless archives

--- backend/dist/countries/countries.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { CountriesService } from './countries.service';
-export declare class CountriesController {
-    private readonly countriesService;
-    constructor(countriesService: CountriesService);
-    getAvailableCountries(): Promise<import("./countries.interface").CountryInfo[]>;
-    getCountryInfo(countryCode: string): Promise<import("./countries.interface").CountryInfoResponse>;
-}
--- backend/dist/countries/countries.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-import { CountryInfo, CountryInfoResponse } from './countries.interface';
-export declare class CountriesService {
-    private readonly httpService;
-    constructor(httpService: HttpService);
-    getAvailableCountries(): Promise<CountryInfo[]>;
-    getCountryInfo(countryCode: string): Promise<CountryInfoResponse>;
-}
